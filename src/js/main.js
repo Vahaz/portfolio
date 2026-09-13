9fa761b45f528a55262ce80b4f7cb2a36bd6ce71
Vahaz <47829911+Vahaz@users.noreply.github.com>
2026-09-14 00:13:23 +0200

    Update 3.1.5
    Fix: unused declaration from gsap

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,4 @@
 import "../css/style.css";
 import "basecoat-css/all";
-import { Observer } from "gsap/Observer";
 import { createIcons, icons  } from "lucide";
 window.addEventListener("load", () => { createIcons({ icons }); });
